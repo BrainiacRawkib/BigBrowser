4a3c50e90f8cf18a358580d7f30242f00bbc2bf9
Brainiac Rawkib <brainiacrawkib@gmail.com>
2022-10-19 15:07:52 +0100

    updated BigBrowser.py and .gitignore

--- BigBrowser.py
+++ BigBrowser.py
@@ -1,16 +1,51 @@
 #!/usr/bin/env python3
 
-import os
-import threading
 import argparse
+import logging
+import os
 import shutil
 import subprocess
+import sys
+import threading
+
+from pathlib import Path
+from zipfile import ZipFile
 
 from bs4 import BeautifulSoup
 
 
 PROGRESS = 0
 TOTAL = 0
+
+
+def sanitize_host(host: str):
+    # remove ., :, /, // if exist in host
+
+    if '.' in host:
+        host = ''.join(host.split('.'))
+    if ':' in host:
+        host = ''.join(host.split(':'))
+    if '/' in host:
+        host = ''.join(host.split('/'))
+    if '//' in host:
+        host = ''.join(host.split('//'))
+    return host
+
+
+def execute_nmap(host: str):
+    """execute nmap"""
+    output_file = f'{sanitize_host(host)}.xml'
+    try:
+        subprocess.run(f'sudo nmap -oX {output_file} -sV {host}',
+                       shell=True,
+                       check=True,
+                       capture_output=True,
+                       text=True)
+        return output_file
+    except subprocess.CalledProcessError as e:
+        logging.error(e)
+    finally:
+        subprocess.run(f'sudo rm {output_file}')
 
 
 def extract_nmap_xml(filename):
@@ -53,7 +88,7 @@ def take_screenshots(url_set: list, nb_threads):
             subprocess.call(['phantomjs', '--ssl-protocol=any', '--ignore-ssl-errors=true', '../sc.js', url, sc_file], stdout=devnull, stderr=devnull)
             devnull.close()
         except Exception as exc:
-            print(f"Screenshot exception : {exc}")
+            logging.exception(f"Screenshot exception : {exc}")
 
 
 def generate_report(urls: list, nb_threads: int = 5, report_name: str = "report.html"):
@@ -117,17 +152,63 @@ def generate_report(urls: list, nb_threads: int = 5, report_name: str = "report.
     print(f"[*] Report generated: file:// {os.path.join(os.getcwd(), report_name)}")
 
 
+def get_all_file_paths(directory):
+    # initializing empty file paths list
+    file_paths = []
+
+    # crawling through directory and subdirectories
+    for root, directories, files in os.walk(directory):
+        for filename in files:
+            # join the two strings in order to form the full filepath.
+            filepath = os.path.join(root, filename)
+            file_paths.append(filepath)
+
+    # returning all file paths
+    return file_paths
+
+
+def zip_dir(host: str):
+    # sanitize host
+    host = sanitize_host(host)
+
+    # path to folder which needs to be zipped
+    directory = '.' # current directory then, os.chdir() method is invoked
+
+    # calling function to get all file paths in the directory
+    file_paths = get_all_file_paths(directory)
+
+    # printing the list of all files to be zipped
+    print('Following files will be zipped:')
+    for file_name in file_paths:
+        print(file_name)
+
+    try:
+        # writing files to a zipfile
+        with ZipFile(f'{host}.zip', 'w') as zip:
+            # writing each file one by one
+            for file in file_paths:
+                zip.write(file)
+        print('All files zipped successfully!')
+        print(f"[*] File Location: file:// {os.path.join(os.getcwd())}")
+    finally:
+        # remove all files that do not end with .zip
+        for file_path in file_paths:
+            if not file_path.endswith('.zip'):
+                Path(file_path).unlink(missing_ok=True)
+
+
 def main():
     parser = argparse.ArgumentParser(description="Generates an HTML report with screenshots of all Web applications from an XML nmap scan.")
     parser.add_argument("file", help="Nmap XML output")
     parser.add_argument("-t", "--threads", help="Number of threads")
     parser.add_argument("-o", "--output", help="Name of the generated report")
+    parser.add_argument("host", help="Host name or IP address")
     args = parser.parse_args()
 
     # Open nmap file and extract Web applications URLs
     if not os.path.exists(args.file):
         print(f"File not found: {args.file}")
-        exit(0)
+        sys.exit(0)
 
     with open(args.file, "r") as f:
         for line in f:
@@ -135,7 +216,7 @@ def main():
                 break
         else:
             print("Not a valid nmap XML")
-            exit(1)
+            sys.exit(1)
 
     urls = extract_nmap_xml(args.file)
 
@@ -144,6 +225,7 @@ def main():
     for url in urls:
         print(url)
     print("=" * 50)
+
     global TOTAL
     TOTAL = len(urls)
 
@@ -152,19 +234,27 @@ def main():
     if args.output:
         report_name = args.output
     if os.path.exists(report_name):
-        if input("Folder exists (" + report_name + ") overwrite ?(y/n)") == "y":
-            shutil.rmtree(report_name)
-        else:
-            exit(1)
+        # Recursively delete a directory tree if it exists
+        shutil.rmtree(report_name)
+
+        # if input(f"Folder exists {report_name} overwrite ?(y/n)").lower() == "y":
+        #     shutil.rmtree(report_name)
+        # else:
+        #     sys.exit(1)
+
+    # make directory
     os.makedirs(report_name)
+
+    # change directory
     os.chdir(report_name)
-    
+
     if args.threads:
         nb_threads = int(args.threads)
     else:
         nb_threads = 4
 
-    generate_report(urls, nb_threads, report_name=report_name + ".html")
+    generate_report(urls, nb_threads, report_name=f'{report_name}.html')
+    zip_dir(args.host)
 
 
 if __name__ == "__main__":
